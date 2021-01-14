27840a131306db495e34c342a880f9919c1078c9
velByte <67099502+velByte@users.noreply.github.com>
2021-01-14 17:36:57 +0100

    Create AboutUs page

--- capture-portofolio/src/App.js
+++ capture-portofolio/src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import AboutUs from "./pages/AboutUs.js";
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Film</h1>
+     <AboutUs/>
     </div>
   );
 }
---
+++ capture-portofolio/src/pages/AboutUs.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+//Import AboutSection
+import AboutSection from "../components/AboutSection.js";
+
+
+const AboutUs = () => {
+    return (
+        <div>
+            <AboutSection/> 
+        </div>
+    ); 
+
+}
+
+export default AboutUs;
