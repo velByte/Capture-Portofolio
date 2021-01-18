983cb656badd215200579845c323b823cf233e41
velByte <67099502+velByte@users.noreply.github.com>
2021-01-18 09:55:31 +0100

    Added FAQ Section

---
+++ capture-portofolio/src/components/FaqSection.js
@@ -0,0 +1,45 @@
+import React from "react"; 
+
+
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>Any quastions? <span>FAQ</span></h2>
+            <div className="quastion">
+                <h4>How Do I Start</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, saepe.</p>
+
+                </div>
+            </div>
+            <div className="quastion">
+                <h4>How Do I Start</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, saepe.</p>
+                    
+                </div>
+            </div>
+            <div className="quastion">
+                <h4>How Do I Start</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, saepe.</p>
+                    
+                </div>
+            </div>
+            <div className="quastion">
+                <h4>How Do I Start</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, saepe.</p>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FaqSection;
--- capture-portofolio/src/pages/AboutUs.js
+++ capture-portofolio/src/pages/AboutUs.js
@@ -2,6 +2,7 @@ import React from "react";
 //Import AboutSection
 import AboutSection from "../components/AboutSection.js";
 import ServicesSection from "../components/ServicesSection.js";
+import FaqSection from "../components/FaqSection.js";
 
 
 const AboutUs = () => {
@@ -9,9 +10,8 @@ const AboutUs = () => {
         <div>
             <AboutSection/> 
             <ServicesSection />
+            <FaqSection/> 
         </div>
     ); 
-
 }
-
 export default AboutUs;
