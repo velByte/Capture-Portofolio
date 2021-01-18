80d571e4afb8a21acef0130d826c5a13dd0f80dd
velByte <67099502+velByte@users.noreply.github.com>
2021-01-18 10:46:50 +0100

    Added initial Styles

--- capture-portofolio/src/App.js
+++ capture-portofolio/src/App.js
@@ -1,10 +1,13 @@
 import React from "react"
 import AboutUs from "./pages/AboutUs.js";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle.js";
 
 
 function App() {
   return (
     <div className="App">
+     <GlobalStyle/>
      <AboutUs/>
     </div>
   );
--- capture-portofolio/src/components/AboutSection.js
+++ capture-portofolio/src/components/AboutSection.js
@@ -1,29 +1,71 @@
 import React from "react"; 
 import home1 from "../img/home1.png";
+//Импортираме компонента, преди това трябва да бъде инсталлиреан с npm 
+// npm install styled-conponents
+//Styled
+import styled from "styled-components";
 
 const AboutSection = () => {
     return(
-        <div>
-            <div className="description">
+				//След като сме оформили желаните css параметри, заместваме div компонента
+				//с името на константата. Константата се създава по-долу
+        <About>
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>your <span>dreams</span> come</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contact us for any photography or videography ideas that you have. We have professionals with amaizing skills </p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 <img src={home1} alt="guy with a camera"/>
-            </div>
-        </div>
+            </Image>
+        </About>
     ); 
 }
+
+
+//Styled Component
+//Създаваме константа, която ще бъде използвана като заместител на HTML тага. 
+//Константата трябва да бъде равна на styled точка следвана от името на HTML тага
+//който трябва да замести, в този случай това е DIV таг. 
+//След това следва `и после съвсем нормален CSS. 
+//Добре е да се инсталира styled-componend разширение за VS Code. 
+const About = styled.div`
+    min-height: 90vh;
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+const Description = styled.div`
+    flex: 1; 
+    padding-right: 5rem; 
+    h2{
+        font-weight: lighter;
+    }
+`;
+const Image = styled.div`
+    flex:1;  
+    overflow: hidden;
+    img{
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }  
+`;
+
+const Hide = styled.div`
+    overflow: hidden;
+`;
 
 export default AboutSection;
