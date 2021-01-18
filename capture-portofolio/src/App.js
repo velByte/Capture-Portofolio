3f485d8c09bc1d1114099ab264e2010d0e0953c1
velByte <67099502+velByte@users.noreply.github.com>
2021-01-18 12:50:50 +0100

    Create Nav

--- capture-portofolio/src/App.js
+++ capture-portofolio/src/App.js
@@ -1,13 +1,16 @@
 import React from "react"
 import AboutUs from "./pages/AboutUs.js";
+import Nav from "./components/Nav.js";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle.js";
+
 
 
 function App() {
   return (
     <div className="App">
      <GlobalStyle/>
+     <Nav/> 
      <AboutUs/>
     </div>
   );
