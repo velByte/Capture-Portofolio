7958b4cb2fab55243b07e12d9cc1852c4f82b6a0
velByte <67099502+velByte@users.noreply.github.com>
2021-01-18 09:34:56 +0100

    Added Services Section

--- capture-portofolio/src/pages/AboutUs.js
+++ capture-portofolio/src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react"; 
 //Import AboutSection
 import AboutSection from "../components/AboutSection.js";
+import ServicesSection from "../components/ServicesSection.js";
 
 
 const AboutUs = () => {
     return (
         <div>
             <AboutSection/> 
+            <ServicesSection />
         </div>
     ); 
 
